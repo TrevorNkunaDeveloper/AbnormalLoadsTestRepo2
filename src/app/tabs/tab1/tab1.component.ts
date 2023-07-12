64542559171b16292186008742a26dc06a0e9b6a
TrevorNkunaDeveloper <78139445+TrevorNkunaDeveloper@users.noreply.github.com>
2023-07-12 19:16:56 +0200

    Added more stuff to tab1 component

--- src/app/tabs/tab1/tab1.component.ts
+++ src/app/tabs/tab1/tab1.component.ts
@@ -80,28 +80,4 @@ export class Tab1Component {
     {value:'owner-3',viewValue:'sample owner 4'},
   ];
 
-  folders = [
-    {
-      name: 'Photos',
-      updated: new Date('1/1/16'),
-    },
-    {
-      name: 'Recipes',
-      updated: new Date('1/17/16'),
-    },
-    {
-      name: 'Work',
-      updated: new Date('1/28/16'),
-    }
-  ];
-  notes = [
-    {
-      name: 'Vacation Itinerary',
-      updated: new Date('2/20/16'),
-    },
-    {
-      name: 'Kitchen Remodel',
-      updated: new Date('1/18/16'),
-    }
-  ];
 }
